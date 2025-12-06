7e842dc34ccccb5deb49dc4a0194fe186f5cfac8
Karthikeyan <mydearkarthikeyan@gmail.com>
2025-12-06 13:03:54 +0530

    Add all page routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,17 @@ import { ContactPage } from './pages/ContactPage';
 import { AboutPage } from './pages/AboutPage';
 import { ApiSolutionsPage } from './pages/ApiSolutionsPage';
 import { AiSolutionsPage } from './pages/AiSolutionsPage';
+import { AirlineSolutionsPage } from './pages/AirlineSolutionsPage';
+import { CareersPage } from './pages/CareersPage';
+import { ManpowerPage } from './pages/ManpowerPage';
+import { TermsPage } from './pages/TermsPage';
+import { PrivacyPage } from './pages/PrivacyPage';
+import { ApiDocumentationPage } from './pages/ApiDocumentationPage';
+import { ApiChangelogPage } from './pages/ApiChangelogPage';
+import { ApiStatusPage } from './pages/ApiStatusPage';
+import { SdkGuidePage } from './pages/SdkGuidePage';
+import { IntegrationGuidePage } from './pages/IntegrationGuidePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 import { RegionProvider } from './context/RegionContext';
 import { CurrencyProvider } from './context/CurrencyContext';
 
@@ -26,8 +37,19 @@ function App() {
             <Route path="/documents" element={<DocumentWalletPage />} />
             <Route path="/api-solutions" element={<ApiSolutionsPage />} />
             <Route path="/ai-solutions" element={<AiSolutionsPage />} />
+            <Route path="/airline-solutions" element={<AirlineSolutionsPage />} />
+            <Route path="/careers" element={<CareersPage />} />
+            <Route path="/manpower" element={<ManpowerPage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/about" element={<AboutPage />} />
+            <Route path="/terms" element={<TermsPage />} />
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/api-docs" element={<ApiDocumentationPage />} />
+            <Route path="/api-changelog" element={<ApiChangelogPage />} />
+            <Route path="/api-status" element={<ApiStatusPage />} />
+            <Route path="/sdk-guide" element={<SdkGuidePage />} />
+            <Route path="/integration-guide" element={<IntegrationGuidePage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Layout>
       </CurrencyProvider>
